ff11ef261e0a38e476c6845158c3ecd10bbf25d9
dkicksd <kicks.web@gmail.com>
2025-06-07 13:35:50 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'puzzleefreeto54.mysql.db';
-$db   = 'puzzleefreeto54';
-$user = 'puzzleefreeto54';
-$pass = 'Ckassou54';
+$host = '';
+$db   = '';
+$user = '';
+$pass = '';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
