b795d823be5810da703d9ae59ac60ed22898c0e7
dkicksd <kicks.web@gmail.com>
2025-06-08 17:20:55 +0200

    Fix trailing duplicate tags in index.php

--- index.php
+++ index.php
@@ -346,9 +346,3 @@
   </script>
 </body>
 </html>
-    </div>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/js/main.js"></script>
-</body>
-</html>
